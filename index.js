8a32843b80d86cf93f0ae2efae7d7a8f7e55e0af
Kiefer <kiefer136@hotmail.com>
2016-07-28 14:39:41 -0700

    added routing, templated ejs

--- index.js
+++ index.js
@@ -9,12 +9,18 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(request, res) {
+  res.render('pages/index');
 });
+
+app.get('/rankings', function(request, res) {
+  res.render('pages/rankings');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
 
 
