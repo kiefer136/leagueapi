17ed5d80551f274e3928fc862d5a8a1e0b5e24a4
Kiefer <kiefer136@hotmail.com>
2016-07-30 02:39:15 -0700

    changed hard code to variables

--- public/javascript/rankings.js
+++ public/javascript/rankings.js
@@ -8,29 +8,31 @@ $(function() {
     dataType: 'json',
     data: {},
     success: function (json) {
+      var sum1id = json[sum1].id;
+      var sum2id = json[sum2].id;
       var obj1 = json[sum1].profileIconId;
       var obj2 = json[sum2].profileIconId;
       $('#sum1icon').html('<img src="http://ddragon.leagueoflegends.com/cdn/6.15.1/img/profileicon/'+ obj1 +'.png"><p></p>');
       $('#sum2icon').html('<img src="http://ddragon.leagueoflegends.com/cdn/6.15.1/img/profileicon/'+ obj2 +'.png"><p></p>');
       $('#name1').html(sum1);
       $('#name2').html(sum2);
-    },
-    error: function (XMLHttpRequest, textStatus, errorThrown) {
-      console.log('error')
-    }
-  });
-  $.ajax({
-    url: 'https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/'+ '580645,42870422'+ '/entry?api_key=' + API_KEY,
-    type: 'GET',
-    dataType: 'json',
-    data: {},
-    success: function (json) {
-      var obj1 = json['580645'][0].tier;
-      var obj2 = json['42870422'][0].tier;
-      var div1 = json['580645'][0].entries[0].division;
-      var div2 = json['42870422'][0].entries[0].division;
-      $('#sum1 p').html(obj1+ ' ' +div1)
-      $('#sum2 p').html(obj2+ ' ' +div2)
+      $.ajax({
+        url: 'https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/'+sum1id+','+sum2id+'/entry?api_key=' + API_KEY,
+        type: 'GET',
+        dataType: 'json',
+        data: {},
+        success: function (json) {
+          var tier1 = json[sum1id][0].tier;
+          var tier2 = json[sum2id][0].tier;
+          var div1 = json[sum1id][0].entries[0].division;
+          var div2 = json[sum2id][0].entries[0].division;
+          $('#sum1 p').html(tier1+ ' ' +div1)
+          $('#sum2 p').html(tier2+ ' ' +div2)
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          console.log('error')
+        }
+      });
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
       console.log('error')
